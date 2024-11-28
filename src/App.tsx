a28f72b5024f26466d1ff04a309523a2717236d8
Priyankagithub123 <ppriyankapriyadarshini.19@gmail.com>
2024-11-28 10:07:08 +0530

    app.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import {BrowserRouter as  Router, Routes,Route } from "react-router-dom";
-import { Suspense,lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Loader from "./components/Loader";
-const Dashboard = lazy(()=>import("./pages/Dashboard"));
-const Customer = lazy(()=>import("./pages/Customer"));
-const Product = lazy(()=>import("./pages/Product"));
-const Transaction = lazy(()=>import("./pages/Transaction"));
 
-
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Customer = lazy(() => import("./pages/Customer"));
+const Product = lazy(() => import("./pages/Product"));
+const Transaction = lazy(() => import("./pages/Transaction"));
 
 const App = () => {
   return (
-    <Suspense fallback={<Loader/>}>
     <Router>
-    <Routes>
-          <Route path="./pages/Dashboard" element={<Dashboard/>}/>
-          <Route path="./pages/Customer" element={<Customer/>}/>
-          <Route path="./pages/Product" element={<Product/>}/>
-          <Route path="./pages/Transaction" element={<Transaction/>}/>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/pages/Dashboard" element={<Dashboard />} />
+          <Route path="/pages/Customer" element={<Customer />} />
+          <Route path="/pages/Product" element={<Product />} />
+          <Route path="/pages/Transaction" element={<Transaction />} />
         </Routes>
+      </Suspense>
     </Router>
-    </Suspense>
-  )
-}
+  );
+};
 
-export default App
+export default App;
