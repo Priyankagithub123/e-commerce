2598712ee50bacca66879be727f300af895adb04
Priyankagithub123 <ppriyankapriyadarshini.19@gmail.com>
2024-12-11 12:07:51 +0530

    dashboard main

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,25 @@
+import { FaRegBell } from "react-icons/fa";
 import AdminSidebar from "../components/AdminSidebar"
-
-
+import {BsSearch} from "react-icons/bs";
+import img from "../assets/userpic.png";
 const Dashboard = () => {
+
   return (
-    <div className="adminContainer">
+    <div className="admin-container">
    <AdminSidebar/>
-       <main>bhfhgj</main>
+       <main className="dashboard">
+        <div className="bar">
+          <BsSearch/>
+          <input type="text" placeholder="Search for data,users,docs"/>
+          <FaRegBell/>
+          <img src={img} alt="User"/>
+        </div>
+        <section className="widgetcontainer">
+        
+        </section>
+       </main>
+      
+    
     </div>
   )
 }
